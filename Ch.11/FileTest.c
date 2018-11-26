897ed0cafa4de01a6c4b84b9e5d793b5ac80126f
d_PITHECUSE_b <gihong96@gmail.com>
2018-11-26 15:19:02 +0900

    Update FileTest.c

--- Ch.11/FileTest.c
+++ Ch.11/FileTest.c
@@ -5,13 +5,20 @@ void uppLowCh1();
 void uppLowCh2();
 void fileTest1();
 void fileTest2();
+void fileTest3();
+void fileTest4();
+void fileTest5();
+void fileTest6();
+void fileTest7();
+void buffTest();
 void mkFile();
+void randAccess();
 
 
 void main()
 {
 
-	fileTest2();
+	fileTest6();
 
 }
 
@@ -100,5 +107,183 @@ void fileTest2()
 	fclose(wfp);
 
 	printf("Words : %d\n", count);
+
+}
+
+void fileTest3()
+{
+	char s[50];
+	int count = 0;
+	char tmp;
+	FILE *rfp;
+	rfp = fopen("c:/tmp/data1.txt", "r");
+
+	fgets(s, 50, rfp);
+	printf("Read from file : ");
+	puts(s);
+
+	fseek(rfp, 0, SEEK_SET);
+
+	while (!feof(rfp))
+	{
+		tmp = fgetc(rfp);
+		count++;
+	}
+
+	printf("%d\n", count);
+	fclose(rfp);
+}
+
+void fileTest4()			// 문자수 count...
+{
+	char s[50];
+	int count = 0;
+	char tmp;
+	FILE *rfp;
+	rfp = fopen("c:/tmp/data1.txt", "r");
+
+	fgets(s, 50, rfp);
+	printf("Read from file : ");
+	puts(s);
+
+	fseek(rfp, 0, SEEK_SET);			// 처음 위치로 돌아가자...
+
+	while (!feof(rfp))
+	{
+		tmp = fgetc(rfp);
+		count++;
+	}
+
+	printf("%d\n", count);
+	fclose(rfp);
+
+}
+
+void fileTest5()		// 문자수 count 2...
+{
+
+	char s[50];
+	int count = 0;
+	char tmp, find;
+	FILE *rfp;
+	rfp = fopen("c:/tmp/data1.txt", "r");
+
+	fgets(s, 50, rfp);
+	printf("Read from file : ");
+	puts(s);
+
+	fseek(rfp, 0, SEEK_SET);			// 처음 위치로 돌아가자...
+
+	printf("What to find? ");
+	scanf(" %c", &find);
+
+	while (!feof(rfp))
+	{
+		tmp = fgetc(rfp);
+		if (tmp == find) count++;
+	}
+
+	printf("%d\n", count);
+	fclose(rfp);
+
+}
+
+void fileTest6()		// 실수 더하기
+{
+
+	FILE *rfp;
+	FILE *wfp;
+	float hap = 0;
+	float in;
+	int i;
+
+	rfp = fopen("c:/tmp/inpt.txt", "r");
+	wfp = fopen("c:/tmp/oupt.txt", "w");
+
+	for (i = 0; i < 2; i++)
+	{
+		fscanf(rfp, "%f", &in);
+		hap = hap + in;
+	}
+	
+	fprintf(wfp, "sum : %f\n", hap);
+	fclose(rfp);
+
+}
+
+void fileTest7()		// 복사하기
+{
+
+	char str[200];
+	FILE *rfp;
+	FILE *wfp;
+
+	rfp = fopen("c:/tmp/data1.txt", "r");
+	wfp = fopen("c:/tmp/data2.txt", "w");
+
+	for (;;)
+	{
+		if (feof(rfp))break;
+		fgets(str, 200, rfp);
+		fputs(str, wfp);
+	}
+
+	fclose(rfp);
+	fclose(wfp);
+
+}
+
+void buffTest()
+{
+
+	int i;
+	int buffer[5] = { 10, 20, 30, 40, 50 };
+	FILE *fp = NULL;
+
+	fp = fopen("c:/tmp/binary.bin", "wb");
+	if (!fp)
+	{
+		fprintf(stderr, "binary.bin 파일을 열수 없습니다.");
+		return;
+	}
+
+	fwrite(buffer, sizeof(int), 5, fp);
+	fclose(fp);
+	for (i = 0; i<5; i++)
+		buffer[i] = 0;
+	fp = fopen("c:/tmp/binary.bin", "rb");
+	if (fp == NULL){
+		fprintf(stderr, "binary.bin 파일을 열 수 없습니다.");
+		return 1;
+	}
+
+	fread(buffer, sizeof(int), 5, fp);
+	for (i = 0; i<5; i++)
+		printf("%d ", buffer[i]);
+	fclose(fp);
+
+	return;
+
+}
+
+void randAccess()
+{
+	FILE *fp;
+	char buffer[100];
+
+	fp = fopen("sample.txt", "w");
+	fputs("ABCDEFGHIJKLMNOPQRSTUVWXYZ", fp);
+	fclose(fp);
+
+	fp = fopen("sample.txt", "r");
+
+	fseek(fp, 3, SEEK_SET);
+	printf("fseek(fp, 3, SEEK_SET) = %c \n", fgetc(fp));
+
+	fseek(fp, -1, SEEK_END);
+	printf("fseek(fp, -1, SEEK_END) = %c \n", fgetc(fp));
+
+	fclose(fp);
+	return 0;
 
 }
